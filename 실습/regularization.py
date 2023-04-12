3e772b53bd5a11091b72a8abec3af26f6cbe3bfc
jni613 <dlwndms2544@gmail.com>
2023-04-12 10:11:50 +0900

    Update regularization.py

--- 실습/regularization.py
+++ 실습/regularization.py
@@ -15,6 +15,11 @@ if torch.cuda.is_available():
 else: 
   device = torch.device('cpu')
 
+transform = transforms.Compose(  #  평균값을 빼고, 표준편차로 나누어 줌으로써 각 채널의 픽셀값이 0을 중심으로 분포하도록 정규화
+    [transforms.ToTensor(),
+     transforms.Normalize((0.4914, 0.4882, 0.4465), (0.247, 0.243, 0.261))]  # 첫번째인자 : CIFAR-10 데이터셋의 픽셀값의 평균(mean) 두번째인자 :  CIFAR-10 데이터셋의 픽셀값의 표준편차(standard deviation)
+)
+
 train_dataset = torchvision.datasets.CIFAR10(root="CIFAR10/",
                                              train=True,
                                              transform=transforms.ToTensor(),
@@ -29,31 +34,35 @@ train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_s
 test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size)
 
 class Model(nn.Module):
-  def __init__(self):
+  def __init__(self, drop_prob):
     super(Model, self).__init__()
     self.linear1 = nn.Linear(32*32*3, 256)
     self.linear2 = nn.Linear(256, 128)
     self.linear3 = nn.Linear(128, 10)
 
+    self.dropout = nn.Dropout(drop_prob)  # dropout regularization
     self.activation = nn.Sigmoid()
 
   def forward(self, x):
     z1 = self.linear1(x)
     a1 = self.activation(z1)
+    a1 = self.dropout(a1)
 
     z2 = self.linear2(a1)
     a2 = self.activation(z2)
+    a2 = self.dropout(a2)
 
     z3 = self.linear3(z2)
 
     return z3
 
-model = Model().to(device).train()
-optimizer = optim.SGD(model.parameters(), lr=1) # set optimizer
+model = Model(drop_prob=0.5).to(device).train()
+optimizer = optim.SGD(model.parameters(), lr=1) 
 
 criterion = nn.CrossEntropyLoss()
 
 epochs = 70
+lmbd = 0.003
 
 train_avg_costs = []
 test_avg_costs = []
@@ -68,6 +77,12 @@ for epoch in range(epochs):
     b_x = b_x.view(-1, 32*32*3).to(device)
     logits = model(b_x)
     loss = criterion(logits, b_y.to(device))
+
+    reg = model.linear1.weight.pow(2.0).sum()  # L2 regularization
+    reg += model.linear2.weight.pow(2.0).sum()
+    reg += model.linear3.weight.pow(2.0).sum()
+    
+    loss += lmbd*reg/len(b_x)/2
 
     optimizer.zero_grad()
     loss.backward()
